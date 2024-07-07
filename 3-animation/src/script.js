75d3b8ddade5b995cff2df6567fb5de226b5e94b
Chris Chung <chrisjhchung@gmail.com>
2024-07-07 16:48:52 +1000

    feat: animations

---
+++ 3-animation/src/script.js
@@ -0,0 +1,61 @@
+import * as THREE from "three";
+import gsap from "gsap";
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
+
+// Using the threejs clock
+// const clock = new THREE.Clock();
+
+// gsap has its own tick
+gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 });
+
+// Animations
+const tick = () => {
+  // commented out - threejs method of simple animations
+
+  // to handle speed (we don't want it to be tied to computer's framerate)
+  // get elapsed time (per second)
+  //   const elapsedTime = clock.getElapsedTime();
+
+  // update objects
+  //   mesh.rotation.y = elapsedTime;
+  //   mesh.position.y = Math.sin(elapsedTime);
+  //   mesh.position.x = Math.cos(elapsedTime);
+
+  //   camera.lookAt(mesh.position);
+
+  // render (still needed when using gsap)
+  renderer.render(scene, camera);
+
+  window.requestAnimationFrame(tick);
+};
+
+tick();
